ce352f011f8bae12ffbd44a2f9dadc34ab17e285
christopherorea <c28_09_98@hotmail.com>
2020-08-05 13:48:24 -0500

    Updates

---
+++ precache-manifest.26674badb91854af1edb4eb45caac2c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adb2e1a374a064c168d52e060bc1e40",
+    "url": "/React-seaons/index.html"
+  },
+  {
+    "revision": "8032c60b205b019f3f43",
+    "url": "/React-seaons/static/css/main.ba96ca27.chunk.css"
+  },
+  {
+    "revision": "981d25c2dd4b1cf1283e",
+    "url": "/React-seaons/static/js/2.c0bda374.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-seaons/static/js/2.c0bda374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8032c60b205b019f3f43",
+    "url": "/React-seaons/static/js/main.cb3be9aa.chunk.js"
+  },
+  {
+    "revision": "c06898ea8b3e861446bb",
+    "url": "/React-seaons/static/js/runtime-main.f2d69240.js"
+  }
+]);
